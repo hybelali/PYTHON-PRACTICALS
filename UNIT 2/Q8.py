e211c291be7c99ae6c91869412fa0edf9bd8e23e
hybelali <hybelali10@gmail.com>
2026-03-24 18:25:12 +0530

    Add files via upload

---
+++ UNIT 2/Q8.py
@@ -0,0 +1,16 @@
+class vehicle:
+    def move(self):
+        return "The vehicle is moving"
+    
+class car(vehicle):
+    def carsound(self):
+        return "Vroom Vroom"
+    
+class bike(vehicle):
+    def bikesound(self):
+        return "ting ting"
+    
+vehicles=[car(), bike()]
+
+for v in vehicles:
+    print(v.move())
